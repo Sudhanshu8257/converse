08eea83c00ac1b77700ad75de4b46bf9247143e1
sudhanshu <lohanasudhanshu8257@gmail.com>
2025-10-05 21:01:49 +0530

    Force dynamic for auth routes

--- app/(authentication)/login/page.tsx
+++ app/(authentication)/login/page.tsx
@@ -1,6 +1,8 @@
 import AuthForm from "@/components/AuthForm";
 import React from "react";
 
+export const dynamic = 'force-dynamic'
+
 const loginPage = () => {
   return <AuthForm useAs="login" />;
 };
--- app/(authentication)/register/page.tsx
+++ app/(authentication)/register/page.tsx
@@ -1,5 +1,7 @@
 import AuthForm from "@/components/AuthForm"
 
+export const dynamic = 'force-dynamic'
+
 const registrationPage = () => {
   return (
     <AuthForm useAs="register" />
