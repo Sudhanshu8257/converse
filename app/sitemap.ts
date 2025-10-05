09724351c15c305dbe729cf3e524f8b27f997d1e
sudhanshu <lohanasudhanshu8257@gmail.com>
2025-10-05 21:47:14 +0530

    added routes in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,13 +1,32 @@
+import { getAllPersonalities } from "@/actions/chatAction";
+import { Personality } from "@/lib/types";
 import { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const [celebs] = await Promise.all([getAllPersonalities({})]);
+  const celebEntries: MetadataRoute.Sitemap = celebs.data.map(
+    (celeb: Personality) => {
+      const formattedTitle = celeb.fullName
+        .trim()
+        .replaceAll(" ", "-")
+        .replaceAll(".", "");
+      const lowercaseformattedTitle = formattedTitle.toLowerCase();
+      const decodedTitle = decodeURIComponent(lowercaseformattedTitle);
+      return {
+        url: `${process.env.BASE_URL}/personality/${escapeXml(decodedTitle)}`,
+        lastModified: new Date() 
+      };
+    }
+  );
   return [
     { url: `${process.env.BASE_URL}`, lastModified: new Date() },
     { url: `${process.env.BASE_URL}/chat` },
+    { url: `${process.env.BASE_URL}/celebrities` },
     { url: `${process.env.BASE_URL}/login` },
     { url: `${process.env.BASE_URL}/register` },
     { url: `${process.env.BASE_URL}/about` },
     { url: `${process.env.BASE_URL}/my-projects` },
+    ...celebEntries
   ];
 }
 
